9ef1ce99d0a19b0fbe83cfb84bedc992598020b5
aren talb <aren.talb00@gmail.com>
2023-12-14 18:01:32 +0300

    Add firebase to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,15 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp({"projectId":"angular-general-practice","appId":"1:905374891134:web:4316e0139e2fdbd872ac2f","storageBucket":"angular-general-practice.appspot.com","apiKey":"AIzaSyDAEjTpz8Jwhymfif2fVbCrXAuDsfHfzeQ","authDomain":"angular-general-practice.firebaseapp.com","messagingSenderId":"905374891134"})),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideDatabase(() => getDatabase())
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyDAEjTpz8Jwhymfif2fVbCrXAuDsfHfzeQ",
+    authDomain: "angular-general-practice.firebaseapp.com",
+    projectId: "angular-general-practice",
+    storageBucket: "angular-general-practice.appspot.com",
+    messagingSenderId: "905374891134",
+    appId: "1:905374891134:web:4316e0139e2fdbd872ac2f"
+  }
+};
