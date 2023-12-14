82bb66295449bbab3c65bae4b5fba0574011e2c1
aren talb <aren.talb00@gmail.com>
2023-12-14 17:18:49 +0300

    Add firebase to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"angular-general-practice","appId":"1:905374891134:web:4316e0139e2fdbd872ac2f","storageBucket":"angular-general-practice.appspot.com","apiKey":"AIzaSyDAEjTpz8Jwhymfif2fVbCrXAuDsfHfzeQ","authDomain":"angular-general-practice.firebaseapp.com","messagingSenderId":"905374891134"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
