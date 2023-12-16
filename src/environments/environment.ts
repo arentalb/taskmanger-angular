8707b11acdce107360d557beb2da5a2fdecfb407
aren talb <aren.talb00@gmail.com>
2023-12-16 16:13:51 +0300

    Change firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,10 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyDAEjTpz8Jwhymfif2fVbCrXAuDsfHfzeQ",
-    authDomain: "angular-general-practice.firebaseapp.com",
-    databaseURL: "https://angular-general-practice-default-rtdb.firebaseio.com",
-    projectId: "angular-general-practice",
-    storageBucket: "angular-general-practice.appspot.com",
-    messagingSenderId: "905374891134",
-    appId: "1:905374891134:web:4316e0139e2fdbd872ac2f"
+    apiKey: "AIzaSyB_YbljopkzyuCOwY70YgFomD83eD-PX8M",
+    authDomain: "taskmanger-30570.firebaseapp.com",
+    projectId: "taskmanger-30570",
+    storageBucket: "taskmanger-30570.appspot.com",
+    messagingSenderId: "635779682320",
+    appId: "1:635779682320:web:6d9053c3ad913811437a95"
   }
 };
