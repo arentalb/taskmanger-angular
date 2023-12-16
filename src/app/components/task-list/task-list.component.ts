7f8d0c1f61e4566fcfeff606171e693c43a6585a
aren talb <aren.talb00@gmail.com>
2023-12-16 12:24:36 +0300

    Add a message when user apply a filter or the is no task in teh database

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -12,7 +12,7 @@ import {AuthService} from "../../services/auth.service";
 export class TaskListComponent implements OnInit {
   tasks: Task[] = []
   isLoading = true
-
+  filterApplyed :boolean = false
   constructor(private firebaseService: FirebaseService ,private authService :AuthService) {
 
   }
@@ -36,11 +36,14 @@ export class TaskListComponent implements OnInit {
 
   applyFilter(event: TaskState) {
     if (event === null ){
+      this.filterApplyed = false
       this.getTasks()
 
     }else {
       this.firebaseService.applyFilter(event).subscribe((tasks)=>{
         this.tasks = tasks
+        this.filterApplyed = true
+        console.log(this.tasks)
       })
     }
 
